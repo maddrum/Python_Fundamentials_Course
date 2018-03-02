e9d313cb4ca6cbb1f113e8db8fe26fab37a17c87
maddrum <maddrum9@gmail.com>
2018-03-02 19:11:21 +0200

    Lecture 8

--- Lecture 8/13. Commits.py
+++ Lecture 8/13. Commits.py
@@ -9,12 +9,12 @@ class Commit:
         self.deletions = int(deletions)
 
 
-pattern = r'https://github.com/(?P<user>[A-Za-z\d-]+)/(?P<repo>[\w_-]+)/commit' \
-          r'(?P<hex>[a-f\d]{40}),(?P<message>[^\n,]+),(?P<addition>\d+),(?P<deletion>\d+)'
+pattern = r'https://github\.com/(?P<user>[A-Za-z\d-]+)/(?P<repo>[A-Za-z_-]+)/commit/(?P<hex>[a-f\d]{40}),(?P<message>[^\n,]+),(?P<addition>\d+),(?P<deletion>\d+)'
 regex = re.compile(pattern)
-data_dict = {}
+username_repo = {}
 while True:
     temp_arr = []
+    repo_data = {}
     input_data = input()
     if input_data == "git push":
         break
@@ -26,23 +26,26 @@ while True:
         message_in = url.group('message')
         addition_in = url.group('addition')
         deletion_in = url.group('deletion')
-        key = username + "+++" + repo_in
         data_class = Commit(hex_str=hex_str_in, message=message_in, additions=addition_in, deletions=deletion_in)
-        if key in data_dict:
-            temp_arr = data_dict[key]
+        if username in username_repo:
+            repo_data = username_repo[username]
+        if repo_in in repo_data:
+            temp_arr = repo_data[repo_in]
         temp_arr.append(data_class)
-        data_dict[key] = temp_arr
-for item in sorted(data_dict):
-    username, repo = item.split('+++')
-    print(username + ":")
-    print(f'  {repo}:')
-    commits = data_dict[item]
-    total_additions = 0
-    total_deletions = 0
-    for commit in commits:
-        print(
-            f'    commit {commit.hex_str}: {commit.message} ({commit.additions} additions, '
-            f'{commit.deletions} deletions)')
-        total_additions += commit.additions
-        total_deletions += commit.deletions
-    print(f'    Total: {total_additions} additions, {total_deletions} deletions')
+        repo_data[repo_in] = temp_arr
+        username_repo[username] = repo_data
+for item in sorted(username_repo):
+    print(item + ":")
+    repos = username_repo[item]
+    for repo in sorted(repos):
+        print(f'  {repo}:')
+        commits = repos[repo]
+        total_additions = 0
+        total_deletions = 0
+        for commit in commits:
+            print(
+                f'    commit {commit.hex_str}: {commit.message} ({commit.additions} additions, '
+                f'{commit.deletions} deletions)')
+            total_additions += commit.additions
+            total_deletions += commit.deletions
+        print(f'    Total: {total_additions} additions, {total_deletions} deletions')
